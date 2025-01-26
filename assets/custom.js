65cd28c76e414fa5ea6f51c27ea03401087be12d
Gastrodia <3025822868@qq.com>
2025-01-26 17:51:09 +0800

    feat: install giscus

--- assets/custom.js
+++ assets/custom.js
@@ -5,4 +5,23 @@ function onLoad() {
     chapterItems.forEach((item) => {
         item.classList.add('expanded');
     });
+
+
+    const script = document.createElement("script")
+    script.type = "text/javascript";
+    script.src = "https://giscus.app/client.js";
+    script.async = true;
+    script.crossOrigin = "anonymous";
+    script.setAttribute("data-repo", "gastrodia/note-rust");
+    script.setAttribute("data-repo-id", "R_kgDONvZAcw");
+    script.setAttribute("data-category", "General");
+    script.setAttribute("data-category-id", "DIC_kwDONvZAc84CmWmK");
+    script.setAttribute("data-mapping", "pathname");
+    script.setAttribute("data-strict", "0");
+    script.setAttribute("data-reactions-enabled", "1");
+    script.setAttribute("data-emit-metadata", "0");
+    script.setAttribute("data-input-position", "top");
+    script.setAttribute("data-theme", "preferred_color_scheme");
+    script.setAttribute("data-lang", "zh-CN");
+    document.querySelector("main").appendChild(script);
 }
